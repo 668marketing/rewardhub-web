8a6201261e43f3e09ff44a614166c2ce9fb8942a
Lim Zi Peng <limzipeng@LZPMacBook-Air.local>
2026-07-30 17:32:38 +0800

    Update PWA version

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,27 +24,27 @@ export const metadata: Metadata = {
   manifest: "/manifest.webmanifest",
 
   icons: {
-    icon: [
-      {
-        url: "/icons/icon-192.png",
-        sizes: "192x192",
-        type: "image/png",
-      },
-      {
-        url: "/icons/icon-512.png",
-        sizes: "512x512",
-        type: "image/png",
-      },
-    ],
+  icon: [
+    {
+      url: "/icons/icon-192.png?v=2",
+      sizes: "192x192",
+      type: "image/png",
+    },
+    {
+      url: "/icons/icon-512.png?v=2",
+      sizes: "512x512",
+      type: "image/png",
+    },
+  ],
 
-    apple: [
-      {
-        url: "/icons/apple-touch-icon.png",
-        sizes: "180x180",
-        type: "image/png",
-      },
-    ],
-  },
+  apple: [
+    {
+      url: "/icons/rewardhub-apple-icon-v2.png",
+      sizes: "180x180",
+      type: "image/png",
+    },
+  ],
+},
 
   appleWebApp: {
     capable: true,
